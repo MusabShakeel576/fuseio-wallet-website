ed915c4b5306f9cc73682bb4eb444f6256b8e063
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-07-10 12:31:22 +0000

    Add isAddress check in QrReader

--- components/qr-reader.tsx
+++ components/qr-reader.tsx
@@ -2,14 +2,24 @@
 
 import { useWalletStore, useQrStore } from '@/lib/stores'
 import { QrScanner } from '@yudiel/react-qr-scanner'
+import { isAddress } from 'ethers'
+import { useToast } from '@/components/ui/use-toast'
 
 export default function QrReader() {
   const addAddress = useWalletStore((state) => state.addAddress)
   const setIsQrReaderOpen = useQrStore((state) => state.setIsQrReaderOpen)
+  const { toast } = useToast()
 
   return (
     <QrScanner
       onDecode={(result) => {
+        if(!isAddress(result)) {
+          setIsQrReaderOpen(false)
+          return toast({
+            variant: "destructive",
+            description: "Address is incorrect",
+          })
+        }
         addAddress(result)
         setIsQrReaderOpen(false)
       }}
