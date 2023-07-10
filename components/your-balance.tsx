071d44938ce4ec6f7ad3b35598f8a32f6cf23d17
Musab Shakil <46605319+MusabShakeel576@users.noreply.github.com>
2023-07-10 15:27:57 +0000

    Add change in balance

--- components/your-balance.tsx
+++ components/your-balance.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { useEffect } from "react"
-import { MoveDown } from "lucide-react"
+import { MoveDown, MoveUp } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import * as Primitive from "@radix-ui/react-dialog"
 import { useMediaQuery } from 'usehooks-ts'
@@ -25,6 +25,7 @@ ReceiveButton.displayName = "ReceiveButton"
 export default function YourBalance() {
   const matches = useMediaQuery('(min-width: 640px)')
   const address = useWalletStore((state) => state.address)
+  const previousBalance = useWalletStore((state) => state.previousBalance)
   const balance = useWalletStore((state) => state.balance)
   const addPreviousBalance = useWalletStore((state) => state.addPreviousBalance)
   const addBalance = useWalletStore((state) => state.addBalance)
@@ -49,11 +50,19 @@ export default function YourBalance() {
         <div className="font-extrabold">Your Balance</div>
         {
           balance ?
-            <div className="flex flex-row items-center">
+            <div className="flex flex-row items-center space-x-2">
               <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
                 ${new Intl.NumberFormat().format(balance)}
               </h3>
-              <span>+0%</span>
+              {
+                ((balance - previousBalance) / previousBalance) * 100 >= 0 ?
+                  <span className="flex flex-row items-center text-green-400">
+                    +{Math.trunc(((balance - previousBalance) / previousBalance) * 100)}% <MoveUp className="mr-2 h-3 w-3" />
+                  </span> :
+                  <span className="flex flex-row items-center text-red-400">
+                    -{Math.trunc(((balance - previousBalance) / previousBalance) * 100)}% <MoveDown className="mr-2 h-3 w-3" />
+                  </span>
+              }
             </div> :
             <Skeleton className="w-[120px] h-[30px] rounded-full" />
         }
